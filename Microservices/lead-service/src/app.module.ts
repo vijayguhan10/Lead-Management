a61410c4d75a547ebb1cc7930ffd4786febba16b
vijayguhan10 <vijayguhan10@gmail.com>
2025-08-02 14:50:11 +0530

    comitting with the changes of the updated lead services Initial setup

--- Microservices/lead-service/src/app.module.ts
+++ Microservices/lead-service/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { LeadModule } from './lead/lead.module';
 
 @Module({
-  imports: [LeadModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://vijayguhan10:TN30e4230!@cluster0.swhz00z.mongodb.net/lead-service',
+    
+    ),
+    LeadModule,
+  ],
   controllers: [],
   providers: [],
 })
--- Microservices/lead-service/src/lead/lead.module.ts
+++ Microservices/lead-service/src/lead/lead.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { LeadController } from './lead.controller';
 import { LeadService } from './lead.service';
+import { Lead, LeadSchema } from './schema/lead.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Lead.name, schema: LeadSchema }]),
+  ],
   controllers: [LeadController],
-  providers: [LeadService]
+  providers: [LeadService],
 })
 export class LeadModule {}
