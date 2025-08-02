afcd9c5286dcbb31e103f8d651d21ecbb781f7c2
vijayguhan10 <vijayguhan10@gmail.com>
2025-08-02 09:35:56 +0530

    comitting with the changes of the updated Auth-services with all endpoints

--- Microservices/auth-service/src/app.module.ts
+++ Microservices/auth-service/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://vijayguhan10:TN30e4230!@cluster0.swhz00z.mongodb.net/auth-service',
+    ),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- Microservices/auth-service/src/auth/auth.module.ts
+++ Microservices/auth-service/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { Auth, AuthSchema } from './Schema/auth.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Auth.name, schema: AuthSchema }]),
+    JwtModule.register({
+      secret: 'your_jwt_secret', 
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
