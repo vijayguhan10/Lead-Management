a28f755f62a1f83fa13af92260a2a346c791e0fe
vijayguhan10 <vijayguhan10@gmail.com>
2025-08-04 00:40:31 +0530

    change the file names and then the routing

--- client/src/Router/InitialRouter.jsx
+++ client/src/Router/InitialRouter.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Auth from "../components/Auth/Auth";
 import Dashboard from "../components/Admin/Dashboard";
-import RecordingsDashboard from "../components/Call/RecordingsDashboard";
+import IndividualTelecaller from "../components/Telecaller/IndividualTelecaller";
+import TelecallerOverviewPanel from "../components/Telecaller/TelecallerOverviewPanel";
+import CallLogs from "../components/Call/CallLogs";
 const InitialRouter = () => {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Auth />} />
           <Route path="/admin-dashboard" element={<Dashboard />} />
-        <Route path="/recordings" element={<RecordingsDashboard />} />
+        <Route path="/call-logs" element={<CallLogs />} />
+        <Route path="/telecaller" element={<TelecallerOverviewPanel />} />
+        <Route path="/viewtelecaller" element={<IndividualTelecaller />} />
       </Routes>
     </div>
   );
