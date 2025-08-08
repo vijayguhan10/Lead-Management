a5ba6b4776a3f2032c0af960ffc765811e36afb3
vijayguhan10 <vijayguhan10@gmail.com>
2025-08-08 12:58:29 +0530

    checking for the merge requests

--- Microservices/auth-service/src/auth/auth.microservice.controller.ts
+++ Microservices/auth-service/src/auth/auth.microservice.controller.ts
@@ -48,7 +48,6 @@ export class AuthMicroserviceController {
 
       return { hasRole, role: user.role };
     } catch (error) {
-      console.log("Error checking role:", error);
       return {
         hasRole: false,
         error: error.message,
