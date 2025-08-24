647124f716ea8d718da818988c9bed655637f9da
Akilesh <akileshakileshs1234@gmail.com>
2025-08-24 14:48:40 +0530

    Enabled all origins to use auth service for development and test. Then added the organization id along with jwt token returning during admin login

--- Microservices/auth-service/src/main.ts
+++ Microservices/auth-service/src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   // Create the HTTP application
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true, // Allow all origins
+    credentials: true,
+  });
+
   // Create the microservice for inter-service communication
   const microservice = app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
