84e8488a8b20e42c36163283432ff62ca9101f87
vijayguhan10 <vijayguhan10@gmail.com>
2025-09-26 15:16:03 +0530

    updating with the removed security for the s3 bucket

--- Microservices/media-service/src/auth/jwt.guard.ts
+++ Microservices/media-service/src/auth/jwt.guard.ts
@@ -14,7 +14,6 @@ export class JwtAuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
 
     const authHeader = request.headers.authorization;
-    console.log('auth Header:', authHeader);
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
       throw new UnauthorizedException('Missing or invalid authorization token');
     
